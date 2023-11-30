a2210292ffd8ccbe677ebb47d588812283fd183a
Katha-Sikdar <ferdouskatha@gmail.com>
2023-11-30 18:23:44 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/15304140-e7eaadb7-1e85-4481-8a60-997ac6166216?access_key=PMAT-01HGDQQ8QSPR549WRDWPFAGQ0W`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
